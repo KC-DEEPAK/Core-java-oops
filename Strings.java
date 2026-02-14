6b8d653d0e337b990fe68e3abc4bee0fff04948c
KC-DEEPAK <deepakkcdeepu77@gmail.com>
2026-02-14 22:31:36 +0530

    Strings_In_Java

---
+++ Strings.java
@@ -0,0 +1,15 @@
+class Strings
+{
+  public static void main(String args[])
+  {
+    //creating a Strings we can do two types ..
+    //String name=new String("Deepak");
+    String name = "Deepak";
+    name = name + " Kumar ";
+    System.out.println(name);
+  // we are checking that given string is mutable or immutable ...
+  String name_1="sanjay";
+  String name_2="sanjay";
+  System.out.println(name_1==name_2);
+  }
+}
