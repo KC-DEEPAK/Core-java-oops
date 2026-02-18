566c6ab8b1fc07364100f21347d887c8667b7144
KC-DEEPAK <deepakkcdeepu77@gmail.com>
2026-02-18 15:10:53 +0530

    Static_Block

---
+++ Static_Block.java
@@ -0,0 +1,40 @@
+// Static Block which is used for to declear a static variable 
+class Mobile
+{
+  String brand;
+  int price;
+  static String name;
+
+  static
+  {
+    name ="SmartPhone";
+    System.out.println("in the static block..");
+  }
+
+  public Mobile()//constructor
+  {
+    brand =" ";
+    price=200;
+    System.out.println("in constructor");
+  }
+  public void show()
+  {
+    System.out.println(brand + " : "+ price +" : "+ name);
+
+  }
+}
+public class Static_Block
+{
+  public static void main(String args[]) throws ClassNotFoundException
+  {
+    Class.forName("Mobile");// to run sattic block only ..
+    //Class.forName(className:"Mobile");
+    // Mobile obj1 =new Mobile();
+    // obj1.brand="Apple";
+    // obj1.price=1500;
+    // Mobile.name="smartphone";
+
+    // Mobile obj2=new Mobile();
+    // Mobile obj3=new Mobile();
+  }
+}
