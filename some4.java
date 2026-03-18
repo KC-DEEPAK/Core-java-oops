3575805aaf0275417b283f8a9ad07da7233ff8e0
KC-DEEPAK <deepakkcdeepu77@gmail.com>
2026-03-18 22:39:49 +0530

    Annotation_IN_Java

---
+++ some4.java
@@ -0,0 +1,16 @@
+class A
+{
+  public void show()
+  {
+    System.out.println("Hii..");
+
+  }
+}
+public class some4
+{
+  public static void main(String args[])
+  {
+    A obj =new A();
+    obj.show();
+  }
+}
