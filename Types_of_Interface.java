bb206c4b9f9c0f9792df47dcfd5e6adf8090789b
KC-DEEPAK <deepakkcdeepu77@gmail.com>
2026-03-19 22:30:00 +0530

    Types_Of_Interface

---
+++ Types_of_Interface.java
@@ -0,0 +1,21 @@
+//Types of Interfaces In which Java as Three Types of Interfaces ...
+//Such that we have 1) Normal Interface . 2)Functional Interface .3)Marker Interface..
+// We have three in this 
+//1) Normal -> A normal Interface which have a Multiple Interface 
+//interface ABC
+// {
+//   public void show()
+//   public void show1()
+// }When we have a two or more methods are called as Normal 
+
+//2)Functional Interface(SAM->Single Abstract Method)-> IF WE have only One method is known as Functional  ..
+//interface BCD
+// {
+//   public void show3()
+
+// } which as a single method is knows as Functional interface..
+// Marker Interface -> which is blank whith out having any method ..
+// interface SDF
+// {
+//   ---
+// }
