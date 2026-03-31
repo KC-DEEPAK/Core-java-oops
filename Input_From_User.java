c3d8d34cf239c138ee10f4b25f6c801469eed0d5
KC-DEEPAK <deepakkcdeepu77@gmail.com>
2026-03-31 19:15:20 +0530

    UserInput

--- Input_From_User.java
+++ Input_From_User.java
@@ -14,13 +14,12 @@ public class Input_From_User
     BufferedReader bf = new BufferedReader(in);
 
     int num = Integer.parseInt(bf.readLine());//this one is to take a input in the from of Integer
-    String  name = bf.readLine();//This one is for String or charceter ..
+    String  name = bf.readLine();//This one is for String or charceter ...
     System.out.println(name);
     //This is used in older version of the java.....
     Scanner sc = new Scanner(System.in);
     //String name = sc.nextLine();//This one is used to scan a input string ..
     int num = sc.nextInt();
     System.out.println(num);
-
   }
 }
